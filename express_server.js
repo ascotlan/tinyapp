378e1ef2cccb1b24fe9fe5c463a0ffa0e6651f63
ascotlan <antonio.k.scotland@gmail.com>
2023-06-24 16:22:31 +0000

    Update your express server so that the id-longURL key-value pair are saved to the urlDatabase when it receives a POST request to /urls

--- express_server.js
+++ express_server.js
@@ -17,32 +17,32 @@ const urlDatabase = {
 function generateRandomString() {
   let result = "";
   const letters = {
-    1: "a",
-    2: "b",
-    3: "c",
-    4: "d",
-    5: "e",
-    6: "f",
-    7: "g",
-    8: "h",
-    9: "i",
-    10: "j",
-    11: "k",
-    12: "l",
-    13: "m",
-    14: "n",
-    15: "o",
-    16: "p",
-    17: "q",
-    18: "r",
-    19: "s",
-    20: "t",
-    21: "u",
-    22: "v",
-    23: "w",
-    24: "x",
-    25: "y",
-    26: "z",
+    0: "a",
+    1: "b",
+    2: "c",
+    3: "d",
+    4: "e",
+    5: "f",
+    6: "g",
+    7: "h",
+    8: "i",
+    9: "j",
+    10: "k",
+    11: "l",
+    12: "m",
+    13: "n",
+    14: "o",
+    15: "p",
+    16: "q",
+    17: "r",
+    18: "s",
+    19: "t",
+    20: "u",
+    21: "v",
+    22: "w",
+    23: "x",
+    24: "y",
+    25: "z",
   };
 
   const digits = {
@@ -68,7 +68,7 @@ function generateRandomString() {
     const upperOrLowerCase = getRandomInteger(2);
 
     if (letterOrDigit) {
-      let letter = letters[getRandomInteger(27)];
+      let letter = letters[getRandomInteger(26)];
       if (upperOrLowerCase) {
         letter = letter.toUpperCase();
       }
@@ -92,7 +92,8 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body, `ShortURL: ${generateRandomString()}`); // Log the POST request body to the console
+  const id = generateRandomString(); //generate random id string
+  urlDatabase[id] = req.body.longURL; //populate database with new id:LongURL key:value pair
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
